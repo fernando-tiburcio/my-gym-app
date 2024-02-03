13b559dd1794691b91b558dc275d0a92c394a16f
Fernando Tiburcio <fernando.tiburcio@gmail.com>
2024-02-03 16:41:56 -0300

    feat: add workoutdata to index workouts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Ionicons } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 import React from 'react';
 import { Accordion, H3, Paragraph, ScrollView, Separator, Square, Text, YStack } from 'tamagui';
 
@@ -19,31 +20,44 @@ export default function WorkoutsScreen() {
         {workoutData.workout_type}
       </H3>
       <ScrollView scrollEnabled paddingHorizontal="$2">
-        <Accordion type="single" defaultValue="a1" collapsible mt="$4" space="$6">
-          {Object.values(workoutData.workout_specification).map((workout) => {
-            console.log('🚀 ~ {Object.entries ~ workout:', workout.workout_title);
-
+        <Accordion
+          type="single"
+          defaultValue={workoutData.workout_specification[0].workout_title}
+          collapsible
+          mt="$4"
+          space="$6">
+          {workoutData.workout_specification.map((workout) => {
             return (
               <Accordion.Item value={workout.workout_title} key={workout.workout_title}>
-                <Accordion.Trigger flexDirection="row" justifyContent="space-between">
+                <Accordion.Trigger
+                  flexDirection="row"
+                  justifyContent="space-between"
+                  paddingTop={8}
+                  paddingBottom={8}
+                  borderTopLeftRadius={12}
+                  borderTopRightRadius={12}>
                   {({ open }: never) => (
                     <>
                       <Paragraph>{workout.workout_title}</Paragraph>
                       <Square animation="quick" rotate={open ? '180deg' : '0deg'}>
-                        <Ionicons name="chevron-down-circle" size={32} color="#00C896" />
+                        <Ionicons name="chevron-down-circle" size={28} color="#00C896" />
                       </Square>
                     </>
                   )}
                 </Accordion.Trigger>
-                <Accordion.Content>
+                <Accordion.Content
+                  style={{ borderBottomRightRadius: 12, borderBottomLeftRadius: 12 }}>
                   <ScrollView horizontal p="$4" scrollEnabled space="$4">
-                    <WorkoutCard />
-                    <WorkoutCard />
-                    <WorkoutCard />
-                    <WorkoutCard />
-                    <WorkoutCard />
-                    <WorkoutCard />
-                    <WorkoutCard />
+                    {Object.values(workout.exercises).map((exercise) => (
+                      <Link
+                        key={exercise.title}
+                        href={{
+                          pathname: '/workout/workoutDetails',
+                          params: { ...exercise },
+                        }}>
+                        <WorkoutCard key={exercise.title} {...exercise} />
+                      </Link>
+                    ))}
                   </ScrollView>
                 </Accordion.Content>
               </Accordion.Item>
--- components/workoutCard.tsx
+++ components/workoutCard.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import { Card, Progress, Image, Text } from 'tamagui';
 
-export default function WorkoutCard() {
+interface Props {
+  title: string;
+  repetitions: number;
+  series: number;
+  dificulty: string;
+}
+
+export default function WorkoutCard({ title, repetitions, dificulty, series }: Props) {
+  const workoutDificulty = {
+    easy: 33,
+    medium: 66,
+    hard: 80,
+    extreme: 100,
+  }[dificulty!];
+
   return (
-    <Card w="$15" h="$20" ai="center" jc="space-between" pb="$3">
+    <Card
+      w="$15"
+      h="$20"
+      ai="center"
+      jc="space-between"
+      pb="$3"
+      br="$4"
+      bg="#000"
+      overflow="hidden">
       <Image
         source={{
-          width: 204,
-          height: 204,
           uri: require('@/assets/images/agachamento-livre.jpg'),
         }}
+        style={{ width: 204, height: 204 }}
       />
 
-      <Text color="$gray12">Agachamento Livre</Text>
-      <Text color="$gray12">3 x 15 reps.</Text>
+      <Text color="$gray12" textAlign="center" ellipse>
+        {title}
+      </Text>
+      <Text color="$gray12">{`${series} x ${repetitions} reps.`}</Text>
       <Text color="$gray10">Dificuldade</Text>
-      <Progress size="$3" value={75} style={{ backgroundColor: '#00C896', width: '90%' }}>
-        <Progress.Indicator animation="bouncy" />
+      <Progress size="$3" value={workoutDificulty} style={{ width: '90%' }}>
+        <Progress.Indicator animation="quick" style={{ backgroundColor: '#00C896' }} />
       </Progress>
     </Card>
   );
