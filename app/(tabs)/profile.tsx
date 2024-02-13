f2f1e6d8b3f75f024f784c632e409497df5ce890
Fernando Tiburcio <fernando.tiburcio@gmail.com>
2024-02-13 10:49:07 -0300

    feat: add little details on profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,16 @@
 import { Feather } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
-import { H3, Image, Text, XStack, YStack } from 'tamagui';
+import { Avatar, H3, Image, Text, XStack, YStack } from 'tamagui';
 
 export default function ProfileScreen() {
   return (
     <YStack ai="center" pt="$4">
       <H3>Perfil</H3>
       <XStack paddingVertical={80}>
-        <Image
-          source={{ uri: 'https://github.com/fernando-tiburcio.png' }}
-          style={{ width: 260, height: 260, borderRadius: 200 }}
-        />
+        <Avatar circular size="$14">
+          <Avatar.Image src="https://github.com/fernando-tiburcio.png" />
+          <Avatar.Fallback bc="#primaryGreen" />
+        </Avatar>
         <TouchableOpacity>
           <Feather name="camera" size={24} color="#00C896" style={{ marginLeft: -20 }} />
         </TouchableOpacity>
